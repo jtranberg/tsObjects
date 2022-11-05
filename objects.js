bda9b4b32ece04f3c5b2c47b303349a362949ff4
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-11-04 22:18:46 -0700

    note added to objects

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function printName(person) {
     console.log("".concat(person.first, " ").concat(person.last));
 }
 printName({ first: "Thomas", last: "jocabian" });
+var coord = { x: 34, y: 56 }; // return type is variable
+function randcoord() {
+    return { x: Math.random(), y: Math.random() };
+}
+printName({ first: 'mick', last: 'Jagger' });
--- objects.ts
+++ objects.ts
@@ -5,11 +5,17 @@ const dog = {
 
 }
 
-function printName(person: {first: string, last: string}): void {
+function printName(person: {first: string, last: string}): void {         //parameter type annotation
     console.log(`${person.first} ${person.last}`)
 }
 
 printName({first: "Thomas", last: "jocabian"});
 
+let coord: {x: number, y: number} = {x:34, y:56}    // return type is variable
 
+function randcoord(): {x: number, y: number} {        //return type is literal
+    return{ x: Math.random(), y: Math.random() };
+}
+
+printName ({first: 'mick', last: 'Jagger'})
 
